ec9f0476f29b46d5074efa2b4807d36b26fcfa6b
niko945 <nikamdivani11@gmail.com>
2024-11-15 00:35:12 +0100

    kremi

---
+++ day 06/classwork/lessen6.py
@@ -0,0 +1,51 @@
+nam1=30
+nam2=5
+nam3=6
+nam4=10
+
+print(nam1+nam4)
+print(nam1+nam2)
+print(nam1+nam3)
+print(nam1+nam1)
+
+print(nam1-nam4)
+print(nam1-nam3)
+print(nam3-nam2)
+print(nam4-nam3)
+
+print(nam2*nam3)
+print(nam3*nam4)
+print(nam1*nam2)
+print(nam4*nam1)
+
+print(nam1/nam2)
+print(nam3/nam2)
+print(nam4/nam3)
+print(nam1/nam4)
+
+deda=39
+mama=39
+print(deda + mama + 20)
+
+
+name = "Zuka"
+user_name = "Ana" 
+number = 10
+subject = "Math"
+print(userAge) 
+age = 25
+age = age + "years"
+Name = "Zuka"
+print(name)
+full_name = "Zuka K"
+deff = "Function"
+userAge = 20
+
+
+
+
+
+
+
+
+
---
+++ day 06/homework/lesson6.py
@@ -0,0 +1,13 @@
+nam1=40
+print(nam1)
+
+print(nam1+90)
+
+nam2=58
+print(nam2-48)
+
+nam3=40
+print(nam3*9)
+
+nam4=48
+print(nam4/8)
