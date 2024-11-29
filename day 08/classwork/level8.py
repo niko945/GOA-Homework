ea0e3ef828b8c9f38a951acad96a20b82747ae33
niko945 <nikamdivani11@gmail.com>
2024-11-29 21:05:32 +0100

    fuli mindaaa

---
+++ day 08/classwork/level8.py
@@ -0,0 +1,16 @@
+name = (input("please enter your name:  "))
+print(name)
+
+
+name1 = input("enter any word:")
+name2 = input("enter any word just any")
+
+
+print(name1 + name2)
+
+#input aris sheni informaciis shetana
+#output aris rodesac ragac gamogitan inputis shemdeg
+
+
+
+
