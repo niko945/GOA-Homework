504305ff9d4bd8ca5db57ef2b835ea0dea5af7e7
niko945 <nikamdivani11@gmail.com>
2024-12-21 19:13:31 +0100

    exam

---
+++ day 10/classwork/lesson10.py
@@ -0,0 +1,24 @@
+name = (input("please enter your name:" ))
+surname = (input("please enter your surname: "))
+age = int(input("please enter youe age: "))
+number1 = int(input("please enter number1: "))
+number2 = int(input("please enter number2: "))
+
+nam1 = int(input("enter your id: "))
+nam2 = int(input("enter your number"))
+
+print(nam1 + nam2)
+print(name)
+print(surname)
+print(age)
+print(number1)
+print(number2)
+
+
+
+
+
+
+
+
+
