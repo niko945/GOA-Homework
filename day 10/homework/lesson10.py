dde4e3146db18f4a3b927055c309fec117ecb5f0
niko945 <nikamdivani11@gmail.com>
2024-12-22 17:53:29 +0100

    saxeli

---
+++ day 10/homework/lesson10.py
@@ -0,0 +1,22 @@
+name=input(("please enter your name and age: "))
+print(name)
+
+nam1=input("39+10")
+nam2=input("39+10")
+nam3=input("16+10")
+nam4=input("14+10")
+
+
+print=(nam1 + nam2 + nam3 + nam4)
+
+
+
+
+num1=10
+num2=6
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
