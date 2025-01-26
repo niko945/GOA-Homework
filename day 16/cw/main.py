3f1bacfa3e09f314ff4ba429a39ab6651371b464
niko945 <nikamdivani11@gmail.com>
2025-01-26 23:43:11 +0100

    ghfvhf

---
+++ day 16/cw/main.py
@@ -0,0 +1,14 @@
+print(4>2 or 3<4)#true
+print(1>4 or 7>4)#false
+print(43>56 or 32<54)#false
+print(4<3 or 3<1)#false
+
+
+print(4>2 and 3<4)#true
+print(1>4 and 7>4)#false
+print(43>56 and 32<54)#false
+print(4<3 and 3<1)#false
+
+
+
+
