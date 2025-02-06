0106531ef35962ba26ce9cbab3d128a4a14d5daf
niko945 <nikamdivani11@gmail.com>
2025-02-06 22:01:48 +0100

    commit

--- day 16/cw/main.py
+++ day 16/cw/main.py
@@ -1,6 +1,6 @@
 print(4>2 or 3<4)#true
-print(1>4 or 7>4)#false
-print(43>56 or 32<54)#false
+print(1>4 or 7>4)#true
+print(43>56 or 32<54)#true
 print(4<3 or 3<1)#false
 
 
---
+++ day 17/hw/main.py
@@ -0,0 +1,27 @@
+for i in range(300, 450, 10):
+     print(i)
+
+
+
+for i in range(200, 500, 2):
+    print(i)
+
+
+
+for i in range(0, 50 , 1):
+    print(i)
+
+
+
+
+for i in range(1000, 1100, 2):
+    print(i)
+
+
+
+for i in range(1, 10):
+    print(i)\
+
+
+for i in range(2000, 2020):
+    print(i)
