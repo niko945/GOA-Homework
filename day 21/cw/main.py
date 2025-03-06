754f30a1f82957a79c1107abffd6c58013a417bb
niko945 <nikamdivani11@gmail.com>
2025-03-06 20:07:13 +0100

    gnbegvbjhvjehgujehguhebgjwgfwgejwfhgwjgfgewjfwjhfe

---
+++ day 21/cw/main.py
@@ -0,0 +1,11 @@
+car = ["BMW","Pegani","Maclare","Mercedes","Bugatti","Tesla","Toyota","Dodge","F1","Opel"]
+print(car)
+
+
+food = ["Salad","Potato","Cucucamber","Hot dog","Burger","Shawarma","Fries","Cheses","Fried chicken","Fried potato"]
+print(food)
+
+num = [1,  2,  3,   4,  5,  6,  7,   8,  9,  10]
+print(num)
+print(num [2:8])
+print(num[5:10])
