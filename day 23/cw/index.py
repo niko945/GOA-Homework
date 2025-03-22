f9d5f8ee12633ddf5676d7598fcf7b3a073cb1ac
niko945 <nikamdivani11@gmail.com>
2025-03-22 16:48:30 +0100

    gehghhegheghegiegiegeghkehgegjege

---
+++ day 23/cw/index.py
@@ -0,0 +1,22 @@
+list = [1, 2, 3, 4, 5,]
+print(list[0])
+print(list[4])
+print(list[1])
+print(list[2])
+print(list[3])
+
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12]
+   #   0   1  2  3  4  5  6  7  8  9
+num[9] = 10
+print(num[9])
+
+
+word = "h, e, l. l, o, w, e, n"
+      # 0  1  2  3  4  5  6  7
+
+
+
+Academy = ["GOA" , "MAGARIA", "NOVATORI" , "IS" , "THE" , "VIRGIN" , "CHAD" , "BEST" , "ACADEMY"]
+          #  0         1           2         3      4         5         6        7         8 
+print(Academy[0] , Academy[3] , Academy[4] , Academy[7] , Academy[8])
---
+++ day 23/hw/main.py
@@ -0,0 +1,28 @@
+car = ["Bugati", "BMW", "merc", "toyota", "jeep", "ford", "opel", "hundai", "tesla", "porche"]
+
+
+list =  [17, 2, 3, 4, 10, 6, 100, 8, 9, 11] 
+      #  0   1  2  3   4  5   6   7  8  9
+list[0] = 1
+print(list[0])
+list[4] = 5
+print(list[4])
+list[6] = 7
+print(list[6])
+list[9] = 10
+print(list[9])
+
+
+
+lists =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+       #  0  1  2  3  4  5  6  7  8  9  
+print(lists[2:7])
+
+
+cars = ["Bugati", "BMW", "merc", "toyota", "jeep", "ford", "opel", "hundai", "tesla", "porche"]
+       #  0         1       2        3        4       5       6        7       8        9   
+
+print(cars[7])
+print(cars[8])
+print(cars[9])
+
