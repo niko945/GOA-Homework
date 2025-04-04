f939af798c43bad46e3d6b3ce8fb45abe6660b3a
niko945 <nikamdivani11@gmail.com>
2025-04-04 20:17:38 +0200

    7ggyfvgf

---
+++ day 25/cw/main.py
@@ -0,0 +1,20 @@
+def greet(hi):
+    print(f" გამარჯობა {hi}.")
+greet("გამარჯობა")
+
+
+
+def name(nika):
+    print(f" გამარჯობა {nika}.")
+name("nika")
+
+
+def num(a, b):
+    print(a + b)
+num(5, 6)
+
+
+
+def food(section):
+    print(f"მე ვჭამ {section}")
+food(apple)
