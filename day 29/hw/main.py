8f17386cc0505a34a983f93fafb549a272f889de
niko945 <nikamdivani11@gmail.com>
2025-05-09 19:52:46 +0200

    ffefefe

--- day 29/hw/main.py
+++ day 29/hw/main.py
@@ -1,6 +1,9 @@
 def sum_of_odds(numbers):
     return sum(num for num in numbers if num % 2 != 0)
 
-# Example usage:
-# my_list = [1, 2, 3, 4, 5]
-# print(sum_of_odds(my_list))  # Output: 9
+def filter_four_letter_names(names):
+        return [name for name in names if len(name) == 4]
+
+def divisible_by_three_and_five(numbers):
+            return [num for num in numbers if num % 3 == 0 and num % 5 == 0]
+
